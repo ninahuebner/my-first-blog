d3b8581794581bb128ea7ac8029007b90f58beca
Nina <you@example.com>
2017-01-28 12:12:16 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,40 @@
+# print("Hello Django!")
+# if 5 > 2:
+#     print('5 is indeed greater than 2')
+# else:
+#     print("5 is not greater than 2")
+# name="Nina"
+# if name == "Mia":
+#     print ("Hey Nina")
+# elif name == "Nina":
+#     print ("Hey Mia")
+# else:
+#     print ("Hey Du")
+# #Change blablabla
+# def hi ():
+#     print("Hi there")
+#     print("How are you?")
+# hi()
+
+# def hi(name):
+#     if name == "Nina":
+#         print ("Hi Nina")
+#     elif name == "Mia":
+#         print("Hi Mia")
+#     else:
+#         print("Hi DU")
+#
+# hi("Mia")
+#
+# def hi(name):
+#     print("Hi" + name + "!")
+#
+# hi("Rob")
+
+def hi(name):
+    print("Hi" + name)
+
+girls=["Regi", "Laura", "Alina"]
+for name in girls:
+    hi(name)
+    print("Next girl")
